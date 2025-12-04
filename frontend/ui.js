5ba5eb9fd849feb41ecf762235a71d36bf358a54
ewd1234 <jacoblorenzen02@gmail.com>
2025-12-04 12:05:32 +0100

    Oprettet loadThemes i uiJs

--- frontend/ui.js
+++ frontend/ui.js
@@ -1,10 +1,8 @@
-window.onload = loadThemes();
-
-async function loadThemes() {
+window.onload = async function loadThemes() {
     const response = await fetch('/api/theme');
     const themes = await response.json();
 
-    const themeSelect = document.getElementById('theme');
+    const themeSelect = document.getElementById('theme-select');
     themes.forEach(theme => {
         const option = document.createElement('option');
         option.value = theme.theme_id;

78d57d93a1ee2104fd993c03e1479a59890d7009
FreddyMail <frederik@coldscale.io>
2025-12-11 10:26:43 +0100

    Refactor song display elements in room view and add console logging for debugging

--- frontend/index.js
+++ frontend/index.js
@@ -73,6 +73,16 @@ async function renderRoom(roomId) {
 
         // Update current song
         let s = data.room.currentSong
+
+        console.log(
+            document.getElementById("room-name"),
+            document.getElementById("song-cover"),
+            document.getElementById("song-title"),
+            document.getElementById("song-artist"),
+            document.getElementById("active-user-count")
+        )
+
+        
         document.getElementById("song-cover").src = s.cover_image;
         document.getElementById("song-title").textContent = s.song_name;
         document.getElementById("song-artist").textContent = s.artist;
